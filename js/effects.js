d14e73d6c611c6265433d224fd7f0ffd19d0bc59
Alberto Blázquez <hola@albertoblazquez.net>
2012-08-08 02:43:13 +0200

    Added scroll to right sections and menu links animation when one of them is selected

--- js/effects.js
+++ js/effects.js
@@ -1,24 +1,62 @@
 
 $(document).on('ready', function() {
 
-	var LUCID    = 'lucid';
+	/* Global jQuery Objects */
+
+	var links    = $('#menu').find('.nav-item');
+	var contents = $('#container').find('.content');
+
+
+	/* Global Constants */
+
 	var HIDDEN   = 'hidden';
-	var links    = $('.menu').find('a');
-	var contents = $('.content');
-		
+	var ACTIVE   = 'active';
+	var DARK     = 'dark';
+	var COLORS   = [ 'blue', 'violet', 'red', 'yellow', 'green' ];
+	var SPEED    = 1000;
+
+
+	/* Animations */
+
+
+	/* Events Definition */
+
 	var linksEvent = function(e) {
+
 		// Local jQuery vars
-		var self = $(this);
-		var num  = self.parent().prevAll().length;
-		var lis  = links.parent();
+		var self   = $(this);
+		var num    = self.prevAll().length;
+		var link   = self.find('a').attr('href');
+		var target = $(link);
 
 		// Showing just the correct '.content' section and the associated menu link
-		contents.addClass(HIDDEN ).eq(num).removeClass(HIDDEN);
-		lis.addClass(LUCID).eq(num).removeClass(LUCID);
+
+		//$('body').stop().animate({ scrollTop : target.offset().top }, SPEED);
+		//contents.hide();
+		//target.show();
+		$('body').stop().animate({ scrollTop : target.offset().top - 50 }, SPEED);
+			/*
+			$.proxy(function() {
+				$('body').css({ scrollTop : this.offset().top });
+			}, target)
+			*/
+		//);
+		
+		links.each(function(i) {
+			var l = links.eq(i);
+			if ( l.hasClass(ACTIVE) ) {
+				l.animate({ width : '5em' }, SPEED/2);
+			}
+
+			l.removeClass(COLORS[i]).removeClass(ACTIVE).addClass(DARK);
+		});
+
+		self.animate({ width : '12em' }, SPEED/2);
+		self.addClass(COLORS[num]).addClass(ACTIVE).removeClass(DARK);
 		
 		// Preventing default event behaviour and 'Nullify' pattern
 		e.preventDefault();
-		self = num = lis = null;
+		self = num = link = target = null;
 	}
 	/*
 	var url      = new Object(window.location.href);
